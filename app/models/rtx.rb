0ebb448d6fbf7983c05e621e1228cc47b7d855c1
王雷 <wanglei@chinacache.com>
2013-05-22 16:06:51 +0800

    update

--- app/models/rtx.rb
+++ app/models/rtx.rb
@@ -1,8 +1,10 @@
 #encoding: utf-8
 class Rtx
-  def self.send_notify(message)
-    url = URI.encode("http://rtx.chinacache.com:8012/sendnotify.cgi?msg="+message+"&receiver=王雷")
-    response = HTTParty.get(url)
-    p response
+  def self.send_notify(msg)
+    receiver = "王雷"
+    Rails.logger.info "/sendnotify.cgi?msg="+URI::escape(msg.encode(Encoding.find("GBK"),Encoding.find("UTF-8")))+"&receiver="+URI::escape(receiver.encode(Encoding.find("GBK"),Encoding.find("UTF-8")))
+    Net::HTTP.start("rtx.chinacache.com", 8012) do |http|
+      http.get("/sendnotify.cgi?msg="+URI::escape(msg.encode(Encoding.find("GBK"),Encoding.find("UTF-8")))+"&receiver="+URI::escape(receiver.encode(Encoding.find("GBK"),Encoding.find("UTF-8"))))
+    end
   end
 end
--- app/workers/pygments_worker.rb
+++ app/workers/pygments_worker.rb
@@ -1,12 +1,13 @@
 class PygmentsWorker
   include Sidekiq::Worker
-  sidekiq_options queue: "high"
+  # sidekiq_options queue: "high"
   # sidekiq_options retry: false
 
   def perform(message)
     p message
     arr = message.split(" ")
-    if arr[0]!="-" and arr[2].startswith("TCP_MISS") and arr[1]>1000 and arr[3]/arr[0]<40 and arr[3]>40
+    # if arr[0]!="-" and arr[2].match("TCP_MISS") and arr[1] > 1000 and arr[3]/arr[0]<40 and arr[3]>40
+    if arr[2].match("TCP_MISS")
       Rtx.send_notify(message)
     end
   end
