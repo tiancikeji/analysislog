38b0ee6901617cba4fb34c834c2f8725f4125fd0
王雷 <wanglei@chinacache.com>
2013-05-22 13:25:53 +0800

    update

--- app/models/rtx.rb
+++ app/models/rtx.rb
@@ -1,5 +1,8 @@
+#encoding: utf-8
 class Rtx
   def self.send_notify(message)
-    
+    url = URI.encode("http://rtx.chinacache.com:8012/sendnotify.cgi?msg="+message+"&receiver=王雷")
+    response = HTTParty.get(url)
+    p response
   end
 end
--- app/workers/pygments_worker.rb
+++ app/workers/pygments_worker.rb
@@ -5,9 +5,9 @@ class PygmentsWorker
 
   def perform(message)
     p message
-    arr = message.split()
-    if arr[1]!="-" 
-      rtx.send_notify(message)
+    arr = message.split(" ")
+    if arr[0]!="-" and arr[2].startswith("TCP_MISS") and arr[1]>1000 and arr[3]/arr[0]<40 and arr[3]>40
+      Rtx.send_notify(message)
     end
   end
 end
